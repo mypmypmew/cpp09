280c2cb919dbf5dba0f5f03ee88184f01bcba2c3
Serhii Nazarov <mypmypmew.dev@gmail.com>
2026-01-03 12:06:18 +0100

    hpp clean up

--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -6,27 +6,28 @@
 #include <deque>
 
 class PmergeMe {
-    public:
-        PmergeMe();
-        PmergeMe(const PmergeMe& other);
-        PmergeMe& operator=(const PmergeMe& other);
-        ~PmergeMe();
+public:
+    PmergeMe();
+    PmergeMe(const PmergeMe& other);
+    PmergeMe& operator=(const PmergeMe& other);
+    ~PmergeMe();
 
-        int run(int argc, char** argv);
+    int run(int argc, char** argv);
 
-    private:
-        std::vector<int> _input;
+private:
+    std::vector<int> _input;
 
-        void printBefore() const;
-        void printAfter(const std::vector<int>& v) const;
-        bool parseInput(int argc, char** argv);
-        void FordJohnsonSortVector(std::vector<int>& v);
-        void FordJohnsonSortDeque(std::deque<int>& d);
+    bool parseInput(int argc, char** argv);
 
-        static void printSequence(const std::string& label,
-                                const std::vector<int>& c,
-                                std::size_t maxPrint = 20);
+    void printBefore() const;
+    void printAfter(const std::vector<int>& v) const;
 
+    void FordJohnsonSortVector(std::vector<int>& v);
+    void FordJohnsonSortDeque(std::deque<int>& d);
+
+    static void printSequence(const std::string& label,
+                              const std::vector<int>& c,
+                              std::size_t maxPrint = 20);
 };
 
 #endif
